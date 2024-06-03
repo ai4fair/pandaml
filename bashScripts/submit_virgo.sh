f138bf40310ede34eee99a34309d531a03f53daa
Nikolai in der Wiesche <n_inde01@uni-muenster.de>
2024-06-03 20:45:44 +0200

    debugging

--- bashScripts/submit_virgo.sh
+++ bashScripts/submit_virgo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sbatch --partition=debug --array=0-2 --job-name=mum5mup5_simulation --output=%x_%a.out --error=%x_%a.err --time=00:10:00 --mail-type=ALL --mail-user=n_inde01@uni-muenster.de --nodes=1 --cpus-per-task=1 --get-user-env ./runSimChain_virgo.sh 10 %x_%a DBoxGEN 1.642 %a
+sbatch --partition=debug --array=0-2 --job-name=mum5mup5_simulation --output=%x_%a.out --error=%x_%a.err --time=00:10:00 --mail-type=ALL --mail-user=n_inde01@uni-muenster.de --nodes=1 --cpus-per-task=1 --get-user-env ./runSimChain_virgo.sh 10 $SLURM_JOB_NAME_$SLURM_ARRAY_TASK_ID DBoxGEN 1.642 $SLURM_ARRAY_TASK_ID
